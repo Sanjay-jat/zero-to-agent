0547e5f0fa96bb1d0f8262740ca9328ae0873342
Sanjay <sanjayjat354339@gmail.com>
2026-07-31 14:23:06 +0000

    Learned about text-loader in Langchain

---
+++ 01-genai-langchain/Document-Loader/Text-Loader/code.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+prompt1=PromptTemplate(
+    template='write only one line poem on {topic}',
+    input_variables=['topic']
+)
+
+
+MODEL_NAME = "gemini-3.6-flash"
+model =ChatGoogleGenerativeAI(model=MODEL_NAME)
+
+parser=StrOutputParser()
+
+loader=TextLoader('document.txt',encoding='utf-8')
+
+docs=loader.load()
+
+#print(docs[0])
+
+print(docs[0].metadata)
+
+chain=prompt1 | model | parser
+result=chain.invoke({'topic':docs[0].page_content})
+print(result)
