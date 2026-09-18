90f4acf40e46e178a9da0eafb777ea6624a99190
Sanjay <sanjayjat354339@gmail.com>
2026-09-18 07:10:27 +0000

    MCP:add resources to server

---
+++ MCP/server-basics/server.py
@@ -0,0 +1,23 @@
+from mcp.server.fastmcp import FastMCP
+mcp=FastMCP("Practice Server")
+
+
+@mcp.tool()
+async def add(a:int,b:int):
+    """Add two numbers."""
+    return a+b
+@mcp.tool()
+async def multiply(a:int,b:int):
+    """Multiply two numbers."""
+    return a*b
+@mcp.resource("greeting://hello")
+async def get_greeting() -> str:
+    """A simple static greeting resource."""
+    return "Hello from the MCP server. This is a resource, not a tool."
+
+
+
+if __name__=="__main__":
+    mcp.run()
+
+
