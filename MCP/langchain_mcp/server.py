b8f749a98c29ade0d09c0d771da2a477549cf765
Sanjay <sanjayjat354339@gmail.com>
2026-09-20 16:44:55 +0000

    Update:server for langchain client added 2 tools

--- MCP/langchain_mcp/server.py
+++ MCP/langchain_mcp/server.py
@@ -13,6 +13,10 @@ async def add(a:int,b:int):
     """Add two numbers."""
     return a+b
 
+@mcp.tool()
+async def multiply(a:int,b:int):
+    """Multiply two numbers."""
+    return a*b
 
 if __name__=="__main__":
     mcp.run()
